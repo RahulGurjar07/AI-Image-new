6f0efe2a4fd5ee782ae4cd1df6d8ac06ad3b53a2
Rahul Gurjar <99276893+RahulGurjar07@users.noreply.github.com>
2025-04-12 11:46:52 +0530

    Update layout.tsx

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,9 +1,17 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="auth">{children}</main>
+    // <main className="auth">{children}</main>
+    <main className="relative h-screen w-full">
+            <div className="absolute size-full">
+              <Image src="/images/bg-img.png" alt="backgound" fill className="size-full" />
+            </div>
+              {children}
+          </main>
   )
 }
 
 export default Layout
+// 
